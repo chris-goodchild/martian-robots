40034fbace2bd47a4bd7b87542b9b6f2060cecd8
Chris Goodchild <chgoodch@microsoft.com>
2016-03-17 14:03:40 +0000

    Committing missing 'scent' feature. Brain FAIL. Robots now avoid areas of the grid where previously deployed robots got lost

--- test/missionFactory.js
+++ test/missionFactory.js
@@ -1,18 +1,31 @@
 const expect = require('chai').expect;
-const missionData = require('../data/mars.json');
+const missionData = require('./test.json');
 const missionFactory = require('../lib/missionFactory');
 
 describe('Mission Factory', function() {
   'use strict';
 
   it('returns a report for each expedition', function() {
-    let mission = missionFactory(missionData.bounds, missionData.expeditions);
+    let mission = missionFactory(missionData[0].bounds, missionData[0].expeditions);
     let reports = mission.launch();
 
     expect(reports).to.deep.equal([
       { position: [1, 1, 'E'] },
-      { position: [3, 3, 'N'], status: 'LOST' },
-      { position: [3, 3, 'N'], status: 'LOST' },
+      { position: [0, 1, 'S'] },
+      { position: [2, 2, 'E'], status: 'LOST' }
     ])
+  });
+
+  describe('when a robot reports that it is "LOST"', function() {
+    it('leaves a scent for other robots', function() {
+      let mission = missionFactory(missionData[1].bounds, missionData[1].expeditions);
+      let reports = mission.launch();
+
+      expect(reports).to.deep.equal([
+        { position: [1, 0, 'W'] },
+        { position: [2, 2, 'N'], status: 'LOST' },
+        { position: [1, 2, 'W'] }
+      ])
+    });
   });
 });
